c49125259f7b6e815b18e4a65a27e799f2dc0a6d
Godfrey Martinez <godmartinz@gmail.com>
2019-06-16 13:55:49 -0700

    all tests pass, and routed correctly

--- server/routes/blogs.js
+++ server/routes/blogs.js
@@ -1,47 +1,96 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const User = require('../models/Blog');
+const Blog = require("../models/Blog");
+const User = require("../models/User");
 
 //Get all Blogs
-router.get('/', (req,res) =>{
-   Blog
-    .find()
-    .then(blogs => {
-        res.status(200).json(blogs); 
-    })
-})
+router.get("/", (req, res) => {
+  Blog.find().then(blogs => {
+    res.status(200).json(blogs);
+  });
+});
 //Get all Featured Blogs
-router.get('/featured', (req,res) =>{
-    Blog
-    .where('featured')
+router.get("/featured", (req, res) => {
+  Blog.where({ featured: true })
     .then(blogs => {
-        res.status(200).json(blogs); 
+      res.status(200).json(blogs);
     })
     .catch(err => res.status(404).send("No featured Blogs found."));
-})
-//Get single Blog
-router.get('/:id', (req,res) =>{
-    Blog
-    .findById(req.params.id)
-    .then(blogs => {
-        // if(!user) res.status(404).send();
-        res.status(200).json(blogs); 
-    })
-    .catch(err => res.status(404).send("Couldnt find that blog"));
-})
-//Create a Blog and assign a User
-router.post('/', (req,res) =>{
-   let tempuser= null;
-   User.findById(req.body.author)
-    .then(user => {
-        
-        dbUser= user;
-        const blogEntry= new Blog(req.body);
-        blogEntry.author= user._id;
-        return blogEntry.save();
-    }).then(blog =>{
-        tempuser.blogs.push(blog);
-        tempuser.save().then(() => res.status(201).json(blog));
-    })
-    .catch(err => res.status(404).send("Couldnt find that blog"));
 });
+//Get single Blog
+router.get("/:id", (req, res) => {
+  const id = req.params.id;
+  Blog.findById(id).then(blogs => {
+    if (!blogs) {
+      res.status(404).send();
+    } else res.status(200).json(blogs);
+  });
+});
+router.post("/", (req,res) => {
+//Create a Blog and assign a User
+let dbUser = null;
+// Fetch the user from the database
+User
+
+    .findById(req.body.author)
+    .then(user => {
+        // Store the fetched user in higher scope variable
+        dbUser = user;
+
+        // Create a blog
+        const newBlog = new Blog(
+            {
+                title: req.body.title,
+                article: req.body.article,
+                published: req.body.published,
+                featured: req.body.featured,
+                author: req.body.author
+            });
+
+        // Bind the user to it
+        newBlog.author = user.id;
+
+        // Save it to the database
+        return newBlog.save();
+    })
+    .then(blog => {
+        // Push the saved blog to the array of blogs associated with the User
+        dbUser.blogs.push(blog);
+
+        // Save the user back to the database and respond to the original HTTP request with a copy of the newly created blog.
+        dbUser.save().then(() => res.status(201).json(blog));
+    });
+});
+//updates a blog
+router.put("/:id", (req, res) => {
+  const id = req.params.id;
+  Blog.findByIdAndUpdate(id, {
+    $set: {
+      title: req.body.title,
+      article: req.body.article,
+      published: req.body.published,
+      featured: req.body.featured
+    }
+  })
+    .exec()
+    .then(result => {
+      console.log(result);
+      res.status(204).send(result);
+    })
+    .catch(err => console.log(err));
+});
+
+// Delete a blog
+router.delete("/:id", (req, res) => {
+  const id = req.params.id;
+  Blog.findByIdAndRemove(id)
+    .exec()
+    .then(blog => {
+      res.status(200).json(blog);
+    })
+    .catch(err => {
+      res.status(400);
+    });
+});
+
+module.exports = router;
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,55 +1,54 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const User = require('../models/User');
+const User = require("../models/User");
 
 //Get all Users
-router.get("/", (req,res)=> {
-    User
-        .find()
-        .then(users => {
-            res.status(200).json(users);
-        });
+router.get("/", (req, res) => {
+  User.find().then(users => {
+    res.status(200).json(users);
+  });
 });
 //Get User By ID
-router.get('/:id', (req,res) => {
-       User 
-        find(req.params.id)
-        .then(users =>{
-            
-            res.status(200).json(users);
-        } )
-        .catch(err => res.status(404).send("Couldn't find that user"));
-})
+router.get("/:id", (req, res) => {
+  const id = req.params.id;
+  User.findById(id).then(users => {
+    if (users) {
+      res.status(200).json(users);
+    } else res.status(404).send("ERROR: NOT FOUND");
+  });
+});
 //Adds a new User
-router.post('/', (req,res) => {
-    const user = new User(req.body);
-    User
-        .save(user)
-        .then(user => {
-           res.status(200).json(user);
-        })
-        .catch(err => res.status(404).send("something went wrong adding a user"));
-})
+router.post("/", (req, res) => {
+  const user = new User({
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email
+  });
+  user.save(function(err, user) {
+    if (user) {
+      res.status(201).send(user);
+    } else console.log(err);
+  });
+  console.log(user);
+});
+
 //Updates an existing user
-router.put('/:id', (req,res) => {
-    
-    User
-        .findByIdAndUpdate(req.params.id)
-        .then(user => {
-            if(!user) res.status(404).send();
-            res.status(202).json(user);
-        })
-        .catch(err => res.status(500).send("did not put"));    
-        })
-router.delete("/:id", (req,res) => {
-    User
-        .findByIdAndRemove(req.params.id)
-        .then(user =>{
-            if(user) res.status(302).send();
-            res.status(404).json(user);
-        })
-        .catch(err => res.status(500).send("Won't delete"));
-})
+router.put("/:id", (req, res) => {
+  User.findByIdAndUpdate(req.params.id, {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email
+  }).then(user => {
+    res.status(204).json(user);
+  });
+});
+router.delete("/:id", (req, res) => {
+  const id = req.params.id;
+  User.findByIdAndRemove(id)
+    .then(user => {
+      res.status(200).json(user);
+    })
+    .catch(err => res.status(500).send("Won't delete"));
+});
 
-
-module.exports  = router;
+module.exports = router;
